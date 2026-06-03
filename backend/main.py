8f8294e8a4c82bc9ffc14cc8ca9855b923fffbd7
omishashree <omishashree@gmail.com>
2026-06-03 12:40:31 +0530

    Initial deployment

--- backend/main.py
+++ backend/main.py
@@ -5,19 +5,55 @@ import numpy as np
 from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 
-from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
+
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import (
+    GlobalAveragePooling2D,
+    Dense,
+    Dropout,
+    BatchNormalization
+)
 
 import tensorflow as tf
 import keras
 
 print("TensorFlow:", tf.__version__)
 print("Keras:", keras.__version__)
+
 # ==============================
-# LOAD TRAINED MODEL
+# BUILD MODEL ARCHITECTURE
 # ==============================
 
-model = load_model("model/steel_defect_mobilenet.h5",compile=False)
+base_model = MobileNetV2(
+    weights=None,
+    include_top=False,
+    input_shape=(224, 224, 3)
+)
+
+base_model.trainable = False
+
+model = Sequential([
+    base_model,
+    GlobalAveragePooling2D(),
+    BatchNormalization(),
+    Dense(256, activation="relu"),
+    Dropout(0.5),
+    Dense(128, activation="relu"),
+    Dropout(0.3),
+    Dense(6, activation="softmax")
+])
+
+# ==============================
+# LOAD MODEL WEIGHTS
+# ==============================
+
+model.load_weights(
+    "model/steel_weights.weights.h5"
+)
+
+print("Model Weights Loaded Successfully")
 
 # ==============================
 # CLASS LABELS
@@ -68,7 +104,6 @@ os.makedirs("uploads", exist_ok=True)
 
 @app.get("/")
 def home():
-
     return {
         "message": "Steel Defect Analyzer API Running Successfully"
     }
@@ -79,27 +114,32 @@ def home():
 
 def predict_defect(img_path):
 
-    # Load image
     img = image.load_img(
         img_path,
         target_size=(IMG_SIZE, IMG_SIZE)
     )
 
-    # Convert to array
     img_array = image.img_to_array(img)
 
-    # Normalize
     img_array = img_array / 255.0
 
-    # Expand dimensions
-    img_array = np.expand_dims(img_array, axis=0)
+    img_array = np.expand_dims(
+        img_array,
+        axis=0
+    )
 
-    # Prediction
-    prediction = model.predict(img_array)
+    prediction = model.predict(
+        img_array,
+        verbose=0
+    )
 
-    predicted_class = class_names[np.argmax(prediction)]
+    predicted_class = class_names[
+        np.argmax(prediction)
+    ]
 
-    confidence = float(np.max(prediction) * 100)
+    confidence = float(
+        np.max(prediction) * 100
+    )
 
     return {
         "prediction": predicted_class,
@@ -113,13 +153,14 @@ def predict_defect(img_path):
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
 
-    # Save uploaded image
     file_path = f"uploads/{file.filename}"
 
     with open(file_path, "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
+        shutil.copyfileobj(
+            file.file,
+            buffer
+        )
 
-    # Predict defect
     result = predict_defect(file_path)
 
     return result
